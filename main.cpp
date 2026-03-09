996245c264c3216f82e7db253dc934f925a36608
Degr3e <glebyal2007@gmail.com>
2026-03-09 13:32:46 +0300

    implementation class WavStruct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 int main()
 {
-  std::cout << "equalizer project\n";
-  return 0;
+  class WavStruct
+  {
+    char chunkID_[4];
+    uint32_t chunkSize_;
+    char format_[4];
+    char subchunk1ID_[4];
+    uint32_t subchunkSize_;
+    uint16_t audioFormat_;
+    uint16_t numChannels_;
+    uint32_t sampleRate_;
+    uint32_t byteRate_;
+    uint16_t blockAlign_;
+    uint16_t bitsPerSample_;
+    char subchunk2ID_[4];
+    uint32_t subchunk2Size_;
+  }
 }

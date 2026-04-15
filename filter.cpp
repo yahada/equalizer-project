04b000597ffdd3abe5128d2d550461789e0e13ac
yahada <niadzy14@gmail.com>
2026-04-15 11:28:30 +0300

    done filter func

--- filter.cpp
+++ filter.cpp
@@ -62,3 +62,117 @@ void equalizer::BiquadFilter::setBiquad(const FilterTypeEnum& filterType, const 
   calcBiquad();
 }
 
+float equalizer::BiquadFilter::process(const float& input)
+{
+  float out = input * inputCurr_ + tmp1_;
+  tmp1_ = (input * inputPrev1_) + tmp2_ - (outputPrev1_ * out);
+  tmp2_ = (input * inputPrev2_) - (outputPrev2_ * out);
+  return out;
+}
+
+void equalizer::BiquadFilter::calcBiquad()
+{
+  double norm = 0;
+  double V = pow(10, fabs(peakGain_) / 20.0);
+  double K = tan(M_PI * fc_);
+  switch (filterType_)
+  {
+  case bq_type_lowpass:
+    norm = 1 / (1 + K / q_ + K * K);
+    inputCurr_ = K * K * norm;
+    inputPrev1_ = 2 * inputCurr_;
+    inputPrev2_ = inputCurr_;
+    outputPrev1_ = 2 * (K * K - 1) * norm;
+    outputPrev2_ = (1 - K / q_ + K * K) * norm;
+    break;
+
+  case bq_type_highpass:
+    norm = 1 / (1 + K / q_ + K * K);
+    inputCurr_ = 1 * norm;
+    inputPrev1_ = -2 * inputCurr_;
+    inputPrev2_ = inputCurr_;
+    outputPrev1_ = 2 * (K * K - 1) * norm;
+    outputPrev2_ = (1 - K / q_ + K * K) * norm;
+    break;
+
+  case bq_type_bandpass:
+    norm = 1 / (1 + K / q_ + K * K);
+    inputCurr_ = K / q_ * norm;
+    inputPrev1_ = 0;
+    inputPrev2_ = -inputCurr_;
+    outputPrev1_ = 2 * (K * K - 1) * norm;
+    outputPrev2_ = (1 - K / q_ + K * K) * norm;
+    break;
+
+  case bq_type_notch:
+    norm = 1 / (1 + K / q_ + K * K);
+    inputCurr_ = (1 + K * K) * norm;
+    inputPrev1_ = 2 * (K * K - 1) * norm;
+    inputPrev2_ = inputCurr_;
+    outputPrev1_ = inputPrev1_;
+    outputPrev2_ = (1 - K / q_ + K * K) * norm;
+    break;
+
+  case bq_type_peak:
+    if (peakGain_ >= 0)
+    {
+      norm = 1 / (1 + 1 / q_ * K + K * K);
+      inputCurr_ = (1 + V / q_ * K + K * K) * norm;
+      inputPrev1_ = 2 * (K * K - 1) * norm;
+      inputPrev2_ = (1 - V / q_ * K + K * K) * norm;
+      outputPrev1_ = inputPrev1_;
+      outputPrev2_ = (1 - 1 / q_ * K + K * K) * norm;
+    }
+    else
+    {
+      norm = 1 / (1 + V / q_ * K + K * K);
+      inputCurr_ = (1 + 1 / q_ * K + K * K) * norm;
+      inputPrev1_ = 2 * (K * K - 1) * norm;
+      inputPrev2_ = (1 - 1 / q_ * K + K * K) * norm;
+      outputPrev1_ = inputPrev1_;
+      outputPrev2_ = (1 - V / q_ * K + K * K) * norm;
+    }
+    break;
+  case bq_type_lowshelf:
+    if (peakGain_ >= 0)
+    {
+      norm = 1 / (1 + sqrt(2) * K + K * K);
+      inputCurr_ = (1 + sqrt(2 * V) * K + V * K * K) * norm;
+      inputPrev1_ = 2 * (V * K * K - 1) * norm;
+      inputPrev2_ = (1 - sqrt(2 * V) * K + V * K * K) * norm;
+      outputPrev1_ = 2 * (K * K - 1) * norm;
+      outputPrev2_ = (1 - sqrt(2) * K + K * K) * norm;
+    }
+    else
+    {
+      norm = 1 / (1 + sqrt(2 * V) * K + V * K * K);
+      inputCurr_ = (1 + sqrt(2) * K + K * K) * norm;
+      inputPrev1_ = 2 * (K * K - 1) * norm;
+      inputPrev2_ = (1 - sqrt(2) * K + K * K) * norm;
+      outputPrev1_ = 2 * (V * K * K - 1) * norm;
+      outputPrev2_ = (1 - sqrt(2 * V) * K + V * K * K) * norm;
+    }
+    break;
+  case bq_type_highshelf:
+    if (peakGain_ >= 0)
+    {
+      norm = 1 / (1 + sqrt(2) * K + K * K);
+      inputCurr_ = (V + sqrt(2 * V) * K + K * K) * norm;
+      inputPrev1_ = 2 * (K * K - V) * norm;
+      inputPrev2_ = (V - sqrt(2 * V) * K + K * K) * norm;
+      outputPrev1_ = 2 * (K * K - 1) * norm;
+      outputPrev2_ = (1 - sqrt(2) * K + K * K) * norm;
+    }
+    else
+    {
+      norm = 1 / (V + sqrt(2 * V) * K + K * K);
+      inputCurr_ = (1 + sqrt(2) * K + K * K) * norm;
+      inputPrev1_ = 2 * (K * K - 1) * norm;
+      inputPrev2_ = (1 - sqrt(2) * K + K * K) * norm;
+      outputPrev1_ = 2 * (K * K - V) * norm;
+      outputPrev2_ = (V - sqrt(2 * V) * K + K * K) * norm;
+    }
+    break;
+  }
+  return;
+}

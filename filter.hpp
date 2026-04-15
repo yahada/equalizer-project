797273ab68986d56f64a0a1bdfaec3a0fae47d48
yahada <niadzy14@gmail.com>
2026-04-15 10:47:38 +0300

    added header for new filter

--- filter.hpp
+++ filter.hpp
@@ -3,24 +3,32 @@
 #include "wav_header.hpp"
 
 namespace equalizer {
-  class LowPass {
-  public:
-    LowPass(float alpha);
-    float proccess(float x);
-  private:
-    float alpha_;
-    float lastY_;
+  enum {
+    bq_type_lowpass = 0,
+    bq_type_highpass,
+    bq_type_bandpass,
+    bq_type_notch,
+    bq_type_peak,
+    bq_type_lowshelf,
+    bq_type_highshelf
   };
 
-  class HighPass {
+  class Biquad {
   public:
-    HighPass(float alpha);
-    float process(float x);
+    Biquad();
+    Biquad(const int& filterType, const double& fc, const double& q, const double& peakGainDB);
+    void setType(const int& filterType);
+    void setQ(const double& q);
+    void setFc(const double& fc);
+    void setPeakGain(const double&  peakGainDB);
+    void setBiquad(const int& filterType, const double& fc, const double& q, const double& peakGainDB);
+    float process(const float& input);
   private:
-      LowPass lp_;
+    void calcBiquad();
+    int filterType_;
+    double inputCurr, inputPrev1_, inputPrev2_, outputPrev1_, outputPrev2_;
+    double Fc_, Q_, peakGain_;
+    double tmp1_, tmp2_;
   };
 }
-
-
-
 #endif
